87349997caae8a115c0591d97369dddc07c73be4
Dmitriy Titov <titov.dmitriy2000@yandex.ru>
2020-12-06 18:48:55 +0300

    version 1.1
    Bugs fixed.

--- src/main/java/dao/dbDAO/PostgresConnectionBuilder.java
+++ src/main/java/dao/dbDAO/PostgresConnectionBuilder.java
@@ -1,5 +1,7 @@
 package dao.dbDAO;
 
+import org.postgresql.util.PSQLException;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -9,7 +11,8 @@ import java.util.Properties;
 
 public class PostgresConnectionBuilder implements ConnectionBuilder {
     private final Properties properties;
-    private final String PATH_TO_CONFIG = "src/main/resources/postgresConfig.properties";
+    private final String PATH_TO_CONFIG = "src/main/resources/localConfig.properties";
+    private final String DB_NAME = "trainsdb";
 
     public PostgresConnectionBuilder() {
         properties = new Properties();
@@ -23,9 +26,38 @@ public class PostgresConnectionBuilder implements ConnectionBuilder {
 
     @Override
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
+        Connection con;
+        try {
+            con = DriverManager.getConnection(
+                properties.getProperty("db.host") + DB_NAME,
+                properties.getProperty("db.login"),
+                properties.getProperty("db.password"));
+        } catch (PSQLException e) {
+            createDBandTables();
+            con = DriverManager.getConnection(
+                properties.getProperty("db.host") + DB_NAME,
+                properties.getProperty("db.login"),
+                properties.getProperty("db.password"));
+        }
+        return con;
+    }
+
+    private void createDBandTables() throws SQLException {
+        var connection = DriverManager.getConnection(
             properties.getProperty("db.host"),
             properties.getProperty("db.login"),
             properties.getProperty("db.password"));
+
+        //create "trainsdb" db
+        var stm = connection.prepareStatement(properties.getProperty("CREATE_DATABASE"));
+        stm.executeUpdate();
+
+        //create routes table
+        stm = connection.prepareStatement(properties.getProperty("CREATE_ROUTES_TABLE"));
+        stm.executeUpdate();
+
+        //create intermediate_stations table
+        stm = connection.prepareStatement(properties.getProperty("CREATE_INTERMEDIATES_STATIONS_TABLE"));
+        stm.executeUpdate();
     }
 }
